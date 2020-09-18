69a51c3f0f3746384c9648e5ec6412b06ad55430
aptmess <improfeo@yandex.ru>
2020-09-18 03:50:11 +0300

    add preprocess | clean changes in code

---
+++ air_modules.py
@@ -0,0 +1,88 @@
+import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 23:30:09 2020
+
+@author: shiro
+"""
+def sum_time(start, duration):
+    st = list(map(lambda x: int(x), start.split(':')))
+    dur = list(map(lambda x: int(x), duration.split(':')))
+    a = [st[0] + dur[0],st[1] + dur[1]]
+    if a[1]>= 60:
+        a[0] += 1
+        a[1] -= 60
+    if a[0]>=24:
+        a[0] -= 24
+    return str(a[0])+':'+str(a[1])
+
+
+def in_minuts(duration):
+    dur  = np.array(list(map(lambda x: int(x), duration.split(':'))))
+    return dur[0]*60 + dur[1]
+
+def in_hours(minuts):
+    h = int(minuts // 60)
+    m = int(minuts % 60)
+    if h < 10:
+        h = '0'+ str(h)
+    else:
+        h = str(h)
+    if m < 10:
+        m = '0'+ str(m)
+    else:
+        m = str(m)
+    return h + ':' + m
+
+def find_night_minuts(fligt_time):
+    night_h1 = list(map(lambda x: in_minuts(x), ['00:00','06:00']))
+    night_h2 = list(map(lambda x: in_minuts(x), ['22:00','23:59']))
+    f = list(map(lambda x: in_minuts(x), fligt_time))
+    delta = 0
+
+    if f[0] < night_h1[1]:
+        if f[1] < night_h1[1]: # весь полёт попал в первую половину ночи
+            delta += f[1] - f[0] # Длительность полёта в минутах
+        else:
+            delta += 6*60 - f[0] 
+    elif f[0] > night_h1[1] and f[0] < night_h2[0]:
+        if f[1] > night_h1[1] and f[1] < night_h2[0]:
+            if f[0] < f[1]:
+                delta += 0  #дневное время
+            else:
+                delta += night_h1[1] + night_h2[1] - night_h2[0] + 1#очень длинный перелёт
+
+        elif f[1] > night_h2[0] and f[1] < night_h2[1]:
+            delta += f[1] - night_h2[0] #часть полёта в вечернее время
+
+        elif f[1] > night_h1[0] and f[1] < night_h1[1]:
+            delta += f[1] + night_h2[1] - night_h2[0] + 1
+
+    elif f[0] > night_h2[0]:
+        if f[1] > night_h2[0] and f[1] < night_h2[1]:
+            delta += f[1]-f[0]
+        elif f[1] < night_h1[1]: 
+            delta += night_h2[1] - f[0] + f[1] + 1
+        elif f[1] > night_h1[1] and f[1] < night_h2[0]:
+            delta += night_h2[1] - f[0] + night_h1[1] + 1
+
+    return delta
+
+def night_percent(start, duration): # start и duration 'hh:mm'
+    d0 = in_minuts(duration)
+    d = in_hours(d0/2)
+    t = sum_time(start, d)
+    h1 = [start, t]
+    t = sum_time(t, '01:30')
+    h2 = [t, sum_time(t,d)]
+    return (find_night_minuts(h1) + find_night_minuts(h2))/d0
+
+
+def is_night_flight(flight):
+    start = flight['Время вылета']
+    duration = flight['Налет']
+    n_pc = night_percent(start, duration)
+    if n_pc > 0.5:
+        return True
+    else:
+        return False
