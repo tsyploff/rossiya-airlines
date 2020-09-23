bf40923cf31db2a490d840a386bd811369e96316
Алексей <43031739+tsyploff@users.noreply.github.com>
2020-09-23 12:39:21 +0400

    import modules some changed

--- russiya_airlines_import.py
+++ russiya_airlines_import.py
@@ -1,8 +1,8 @@
-import air_modules as am
 import numpy as np
 import pandas as pd
 import itertools
 from sklearn.preprocessing import OneHotEncoder
+from air_modules import *
 import json
 
 def is_night_flight(flight): # Является ли связка ночной
@@ -25,7 +25,7 @@ def load_tester(path):
 
 def preprocess(data):
     
-    data['night'] = np.array([am.is_night_flight(data.iloc[i]) * 1 for i in range(data.shape[0])])
+    data['night'] = np.array([is_night_flight(data.iloc[i]) * 1 for i in range(data.shape[0])])
     data_columns = ['Налет', 'Экипаж', 'Тип связи', 'День месяца', 'Тип судна', 'Назначение', 'Время вылета', 'night']
     
     F_columns = ['t', 'p', 'v', 'd', 'a', 's', 'goodbye', 'night']
