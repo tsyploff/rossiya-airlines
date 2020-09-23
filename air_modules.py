b4dbf799b8cae54889450c4d60bbc5a9ee6bbb06
KarpovaSofya <karpova.sofya@yandex.ru>
2020-09-23 10:36:02 +0300

    Add files via upload

--- air_modules.py
+++ air_modules.py
@@ -1,11 +1,24 @@
 import numpy as np
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 17 23:30:09 2020
+import pandas as pd
 
-@author: shiro
-"""
-def sum_time(start, duration):
+def total_time(start, duration): #сложение часов
+    st = list(map(lambda x: int(x), start.split(':')))
+    dur = list(map(lambda x: int(x), duration.split(':')))
+    a = [st[0] + dur[0],st[1] + dur[1]]
+    if a[1]>= 60:
+        a[0] += 1
+        a[1] -= 60
+    if a[0] < 10:
+        h = '0'+ str(a[0])
+    else:
+        h = str(a[0])
+    if a[1] < 10:
+        m = '0'+ str(a[1])
+    else:
+        m = str(a[1])
+    return h + ':' + m
+
+def sum_time(start, duration): #сложение часов с обнулением
     st = list(map(lambda x: int(x), start.split(':')))
     dur = list(map(lambda x: int(x), duration.split(':')))
     a = [st[0] + dur[0],st[1] + dur[1]]
@@ -15,7 +28,6 @@ def sum_time(start, duration):
     if a[0]>=24:
         a[0] -= 24
     return str(a[0])+':'+str(a[1])
-
 
 def in_minuts(duration):
     dur  = np.array(list(map(lambda x: int(x), duration.split(':'))))
@@ -34,7 +46,7 @@ def in_hours(minuts):
         m = str(m)
     return h + ':' + m
 
-def find_night_minuts(fligt_time):
+def find_night_minuts(fligt_time):  # Количество ночных минут полёта
     night_h1 = list(map(lambda x: in_minuts(x), ['00:00','06:00']))
     night_h2 = list(map(lambda x: in_minuts(x), ['22:00','23:59']))
     f = list(map(lambda x: in_minuts(x), fligt_time))
@@ -66,9 +78,9 @@ def find_night_minuts(fligt_time):
         elif f[1] > night_h1[1] and f[1] < night_h2[0]:
             delta += night_h2[1] - f[0] + night_h1[1] + 1
 
-    return delta
+    return delta 
 
-def night_percent(start, duration): # start и duration 'hh:mm'
+def night_percent(start, duration): # start и duration 'hh:mm'   - % от полёта, проходящий в ночное время
     d0 = in_minuts(duration)
     d = in_hours(d0/2)
     t = sum_time(start, d)
@@ -77,10 +89,9 @@ def night_percent(start, duration): # start и duration 'hh:mm'
     h2 = [t, sum_time(t,d)]
     return (find_night_minuts(h1) + find_night_minuts(h2))/d0
 
-
-def is_night_flight(flight):
-    start = flight['Время вылета']
-    duration = flight['Налет']
+def is_night_flight(flight): # Является ли связка ночной
+    start = flight['Время вылета'].iloc[0]
+    duration = flight['Налет'].iloc[0]
     n_pc = night_percent(start, duration)
     if n_pc > 0.5:
         return True
--- russiya_airlines_report.py
+++ russiya_airlines_report.py
@@ -1,114 +1,17 @@
 import os
 
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+from air_modules import*
 
-def total_time(start, duration): #сложение часов
-    st = list(map(lambda x: int(x), start.split(':')))
-    dur = list(map(lambda x: int(x), duration.split(':')))
-    a = [st[0] + dur[0],st[1] + dur[1]]
-    if a[1]>= 60:
-        a[0] += 1
-        a[1] -= 60
-    if a[0] < 10:
-        h = '0'+ str(a[0])
-    else:
-        h = str(a[0])
-    if a[1] < 10:
-        m = '0'+ str(a[1])
-    else:
-        m = str(a[1])
-    return h + ':' + m
-
-def sum_time(start, duration): #сложение часов с обнулением
-    st = list(map(lambda x: int(x), start.split(':')))
-    dur = list(map(lambda x: int(x), duration.split(':')))
-    a = [st[0] + dur[0],st[1] + dur[1]]
-    if a[1]>= 60:
-        a[0] += 1
-        a[1] -= 60
-    if a[0]>=24:
-        a[0] -= 24
-    return str(a[0])+':'+str(a[1])
-
-def in_minuts(duration):
-    dur  = np.array(list(map(lambda x: int(x), duration.split(':'))))
-    return dur[0]*60 + dur[1]
-
-def in_hours(minuts):
-    h = int(minuts // 60)
-    m = int(minuts % 60)
-    if h < 10:
-        h = '0'+ str(h)
-    else:
-        h = str(h)
-    if m < 10:
-        m = '0'+ str(m)
-    else:
-        m = str(m)
-    return h + ':' + m
-
-def find_night_minuts(fligt_time):  # Количество ночных минут полёта
-    night_h1 = list(map(lambda x: in_minuts(x), ['00:00','06:00']))
-    night_h2 = list(map(lambda x: in_minuts(x), ['22:00','23:59']))
-    f = list(map(lambda x: in_minuts(x), fligt_time))
-    delta = 0
-
-    if f[0] < night_h1[1]:
-        if f[1] < night_h1[1]: # весь полёт попал в первую половину ночи
-            delta += f[1] - f[0] # Длительность полёта в минутах
-        else:
-            delta += 6*60 - f[0] 
-    elif f[0] > night_h1[1] and f[0] < night_h2[0]:
-        if f[1] > night_h1[1] and f[1] < night_h2[0]:
-            if f[0] < f[1]:
-                delta += 0  #дневное время
-            else:
-                delta += night_h1[1] + night_h2[1] - night_h2[0] + 1#очень длинный перелёт
-
-        elif f[1] > night_h2[0] and f[1] < night_h2[1]:
-            delta += f[1] - night_h2[0] #часть полёта в вечернее время
-
-        elif f[1] > night_h1[0] and f[1] < night_h1[1]:
-            delta += f[1] + night_h2[1] - night_h2[0] + 1
-
-    elif f[0] > night_h2[0]:
-        if f[1] > night_h2[0] and f[1] < night_h2[1]:
-            delta += f[1]-f[0]
-        elif f[1] < night_h1[1]: 
-            delta += night_h2[1] - f[0] + f[1] + 1
-        elif f[1] > night_h1[1] and f[1] < night_h2[0]:
-            delta += night_h2[1] - f[0] + night_h1[1] + 1
-
-    return delta 
-
-def night_percent(start, duration): # start и duration 'hh:mm'   - % от полёта, проходящий в ночное время
-    d0 = in_minuts(duration)
-    d = in_hours(d0/2)
-    t = sum_time(start, d)
-    h1 = [start, t]
-    t = sum_time(t, '01:30')
-    h2 = [t, sum_time(t,d)]
-    return (find_night_minuts(h1) + find_night_minuts(h2))/d0
-
-def is_night_flight(flight): # Является ли связка ночной
-    start = flight['Время вылета'].iloc[0]
-    duration = flight['Налет'].iloc[0]
-    n_pc = night_percent(start, duration)
-    if n_pc > 0.5:
-        return True
-    else:
-        return False
-
-
-def table_round_trip(solution):
+def table_round_trip(solution, data):
     '''Принимает на вход решение в виде pd.DataFrame, 
     возвращает табличку "распределение связок по группам"
     в виде pd.DataFrame
     >>> print(type(solution))
     <class 'pandas.core.frame.DataFrame'>
     '''
-     a = np.zeros([6,8])
+    a = np.zeros([6,8])
 
     for j in range(len(solution)):
         rout = solution.iloc[j].iloc[0]
@@ -116,7 +19,7 @@ def table_round_trip(solution):
         t = data[data['Связка']==rout]
         t = t[t['Время вылета']==start]
         for i in t['День месяца'].values:
-            group = solution.iloc[j][i]
+            group = solution.iloc[j][str(i)]
             if group == 0:
                 i += 1
             else:
@@ -137,7 +40,8 @@ def table_round_trip(solution):
                 if is_night_flight(d):
                     a[group-1, 7] += 1
     return pd.DataFrame(a, columns=['ВВЛ: БП','ВВЛ: Связки','МВЛ: БП','МВЛ: СВ','СНГ: БП','СНГ: Связки','Всего связок','Всего носных связок'])
-def table_flight_time(solution):
+
+def table_flight_time(solution, data):
     '''Принимает на вход решение в виде pd.DataFrame, 
     возвращает табличку "планируемое время налёта"
     в виде pd.DataFrame
@@ -152,7 +56,7 @@ def table_flight_time(solution):
         t = data[data['Связка']==rout]
         t = t[t['Время вылета']==start]
         for i in t['День месяца'].values:
-            group = solution.iloc[j][i]
+            group = solution.iloc[j][str(i)]
             if group == 0:
                 i += 1
             else:
@@ -177,13 +81,16 @@ def table_flight_time(solution):
         delta += [in_hours(in_minuts(max(h[i])) - in_minuts(min(h[i])))]
     h.loc['delta'] = delta
     return h
-def table_flight_on_direction(solution):
+
+
+def table_flight_on_direction(solution, data):
     '''Принимает на вход решение в виде pd.DataFrame, 
     возвращает табличку "количество связок по направлениям"
     в виде pd.DataFrame
     >>> print(type(solution))
     <class 'pandas.core.frame.DataFrame'>
     '''
+    destin = set(data['Назначение'])
     ans = pd.DataFrame(np.zeros([len(destin),8]), index = destin, columns=['1','2','3','4','5','6','All', 'delta'])
     for j in range(len(solution)):
         rout = solution.iloc[j].iloc[0]
@@ -191,7 +98,7 @@ def table_flight_on_direction(solution):
         t = data[data['Связка']==rout]
         t = t[t['Время вылета']==start]
         for i in t['День месяца'].values:
-            group = solution.iloc[j][i]
+            group = solution.iloc[j][str(i)]
             if group == 0:
                 i += 1
             else:
@@ -208,21 +115,21 @@ def table_flight_on_direction(solution):
 
     return(ans)
 
-def table_flight_on_date(solution):
+def table_flight_on_date(solution, data):
     '''Принимает на вход решение в виде pd.DataFrame, 
     возвращает табличку "число вылетов по датам"
     в виде pd.DataFrame
     >>> print(type(solution))
     <class 'pandas.core.frame.DataFrame'>
     '''
-    ans = pd.DataFrame(np.zeros([len(solution.iloc[0])-3,8]), index = range(1,len(solution.iloc[0])-2), columns=['1','2','3','4','5','6','All', 'delta'])
+    ans = pd.DataFrame(np.zeros([len(solution.iloc[0])-2,8]), index = range(1,len(solution.iloc[0])-1), columns=['1','2','3','4','5','6','All', 'delta'])
     for j in range(len(solution)):
         rout = solution.iloc[j].iloc[0]
         start = solution.iloc[j].iloc[1]
         t = data[data['Связка']==rout]
         t = t[t['Время вылета']==start]
         for i in t['День месяца'].values:
-            group = solution.iloc[j][i]
+            group = solution.iloc[j][str(i)]
             if group == 0:
                 i += 1
             else:
@@ -230,14 +137,14 @@ def table_flight_on_date(solution):
     ans['All'] = ans['1'] + ans['2'] + ans['3'] + ans['4'] + ans['5']+ ans['6']
 
     delta = []
-    for i in range(1,len(solution.iloc[0])-2):
+    for i in range(1,len(solution.iloc[0])-1):
         s = ans.loc[i]
         delta += [max(s['1'],s['2'],s['3'],s['4'],s['5'],s['6']) - min(s['1'],s['2'],s['3'],s['4'],s['5'],s['6'])]
     ans['delta'] = delta
 
     return(ans)
 
-def table_flight_on_plane(solution):
+def table_flight_on_plane(solution, data):
     '''Принимает на вход решение в виде pd.DataFrame, 
     возвращает табличку "количество связок по типу воздушного судна"
     в виде pd.DataFrame
@@ -251,7 +158,7 @@ def table_flight_on_plane(solution):
         t = data[data['Связка']==rout]
         t = t[t['Время вылета']==start]
         for i in t['День месяца'].values:
-            group = solution.iloc[j][i]
+            group = solution.iloc[j][str(i)]
             if group == 0:
                 i += 1
             else:
@@ -289,16 +196,27 @@ def export_tables(title, tables):
     каждый лист именуется названием таблицы'''
     pass
 
-def solution_report(solution, specification):
-    t1 = table_round_trip(solution)
+def solution_report(solution, data):
+    '''Принимает на вход решение в виде pd.DataFrame, 
+    строит все таблицы и экспортирует их в эксель с названием 
     
-    t2 = table_flight_time(solution)
+    "Решение ({}, {}, {}).xlsx".format(specification['Алгоритм'], specification['Сортировка'], specification['Веса критериев'])
+
+    Если такой файл уже существует, в конце добавляется (1). Если уже существует файл с названием 
+    "Решение ({}, {}, {}) (k).xlsx".format(...)
+
+    в конце добавляется (k + 1)
+    file_name = ["Решение ({}, {}, {}).xlsx".format(specification['Алгоритм'], specification['Сортировка'], specification['Веса критериев'])]'''
+    #file_name = ["Решение ({}, {}, {}).xlsx".format(specification['Алгоритм'], specification['Сортировка'], specification['Веса критериев'])]
+    t1 = table_round_trip(solution, data)
     
-    t3 = table_flight_on_direction(solution)
+    t2 = table_flight_time(solution, data)
     
-    t4 = table_flight_on_date(solution)
+    t3 = table_flight_on_direction(solution, data)
     
-    t5 = table_flight_on_plane(solution)
+    t4 = table_flight_on_date(solution, data)
+    
+    t5 = table_flight_on_plane(solution, data)
     
     with pd.ExcelWriter(file_name + '.xlsx') as writer: 
         t1.to_excel(writer, sheet_name = 'Таблица 1')
